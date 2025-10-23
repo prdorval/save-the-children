06f3936f20a29f21617d61b44edd3064aaa44678
Carissa_Dorval <carissa.d.atc@gmail.com>
2025-10-22 22:44:44 -0500

    I added my own page

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return "This is a placeholder for your webapp's opening page."
 
+@app.get("/fromTexas")
+def bycad():
+    return "This is a new page."
+
 
 if __name__ == "__main__":
     app.run()
